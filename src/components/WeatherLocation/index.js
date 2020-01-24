28eb56d94e7c3957a0f7562c5d3429dad3b8f5b3
Miguel Triana <jorge.triana@sercop.gob.ec>
2020-01-24 17:22:04 -0500

    37. Utilización de PropType.Shape

---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+import "./styles.css";
+import {     
+    SUN
+ } from "../../constants/weathers";
+
+ const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: "10 m/s"
+ }
+
+const WeatherLocation = () => (
+    <div className="weatherLocationCont">
+        <Location city={"Ecuador"}></Location>
+        <WeatherData data={data}></WeatherData>
+    </div>
+);
+
+export default WeatherLocation;
