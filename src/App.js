e11641f1d7bafe2bdd5e6090a9f4567d904b0c0d
Miguel Triana <mikesoft241184@gmail.com>
2020-01-26 14:16:17 -0500

    73. Uso de Grid, Row, Col y vh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Col, Row  } from "react-flexbox-grid";
 import './App.css';
 import LocationList from './components/LocationList';
 
@@ -11,20 +12,28 @@ const cities = [
   "Lima,pe"
 ];
 
-
-
 class App extends Component {
   handleSelectionLocation = city => {
     console.log(`handleSelectionLocation ${city}`);
   }
   render() {
     return (
-      <div className="App">      
-        <LocationList 
-          cities={cities}
-          onSelectedLocation={this.handleSelectionLocation}
-        />
-      </div>
+      <Grid>
+        <Row>
+          Titulo
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList 
+              cities={cities}
+              onSelectedLocation={this.handleSelectionLocation}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="details"></div>
+          </Col>
+        </Row>        
+      </Grid>      
     );
   }
 }
