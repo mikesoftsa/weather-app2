3d9188f255018b6c0d9b291985512e1be82f8838
Miguel Triana <mikesoft241184@gmail.com>
2020-01-25 20:52:26 -0500

    45. Utilización de valores retorno de Fetch

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -7,6 +7,12 @@ import {
     WINDY
  } from "../../constants/weathers";
 
+ const location = "Buenos Aires,ar";
+ const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+ const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+ const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
  const data = {
     temperature: 5,
     weatherState: SUN,
@@ -33,10 +39,15 @@ class WeatherLocation extends Component {
     }
 
     handleUpdateClick = () => {
+        fetch(api_weather).then(resolve => {
+            console.log(resolve)
+            return resolve.json()
+        }).then(data => {
+            console.log(data);
+        });
         console.log("actualizado");
 
         this.setState({
-            city: "Buenos Aires!",
             data: data2
         });
     }
