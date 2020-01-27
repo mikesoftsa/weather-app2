3ab367ae47840cdacb1088e2dcf87da0cf8a4ca8
Miguel Triana <jorge.triana@sercop.gob.ec>
2020-01-27 16:15:16 -0500

    88. Creación componente ForecastItem

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,5 +1,6 @@
 import React, { Component  } from "react";
 import PropTypes from "prop-types";
+import ForecastItem from "./ForecastItem";
 import "./styles.css";
 
 class ForecastExtended extends Component {
@@ -8,6 +9,7 @@ class ForecastExtended extends Component {
         return (
             <div>
                 <h2 className="forecast-title">Pronóstico extendido para {city}</h2>
+                <ForecastItem></ForecastItem>
             </div>
         )
     }
---
+++ src/components/ForecastItem/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ForecastItem = () => (
+    <div>Item</div>
+);
+
+export default ForecastItem;
