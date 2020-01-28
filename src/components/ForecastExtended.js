508fdf75851b1c13ec0dc5e3198cadf22822531a
Miguel Triana <mikesoft241184@gmail.com>
2020-01-27 21:42:54 -0500

    93. Api OpenWeatherMap para pronostico extendido: Forecast

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -18,6 +18,10 @@ const days = [
     wind: 'normal'
 }*/
 
+export const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+export const url= "http://api.openweathermap.org/data/2.5/forecast";
+
+
 class ForecastExtended extends Component {
     
     constructor() {
@@ -28,6 +32,11 @@ class ForecastExtended extends Component {
         }
 
     }
+
+    componentDidMount() {
+        
+    }
+    
 
     renderForecastItenDay() {
         return "Render Items";
