58e79847234c5368d7d7af263ab444cd9e350357
Miguel Triana <mikesoft241184@gmail.com>
2020-01-29 00:53:52 -0500

    124. Entendiendo el reducer

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -1,10 +1,9 @@
 import { SET_CITY } from "./../actions";
-export const city = (state, action) => {
+export const city = (state = {}, action) => {
     switch (action.type) {
         case SET_CITY:
-            return {...state, city: action.value}                
+            return {...state, city: action.payload}                
         default:
-            break;
-    }
-    return state;
+            return state;
+    }    
 }
