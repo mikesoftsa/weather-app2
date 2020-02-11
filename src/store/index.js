16802da59922a71c900b6ca5838e01e2c6129097
Miguel Triana <jorge.triana@sercop.gob.ec>
2020-02-11 17:06:56 -0500

    133. Utilización de combineReducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import { city } from "./../reducers/city";
+import reducers from "./../reducers";
 
 const initialState = {
     city: "Quito,ec"
@@ -9,7 +9,7 @@ const initialState = {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    city, 
+    reducers, 
     initialState,
     composeEnhancers(applyMiddleware(thunk))
 );
