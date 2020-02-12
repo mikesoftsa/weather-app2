c88ef25adfd778c6dc1ed413e89247bd7d01569a
Miguel Triana <mikesoft241184@gmail.com>
2020-02-11 22:02:26 -0500

    134. Simplificación de class component a functional component

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,100 +1,36 @@
-import React, { Component  } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ForecastItem from "./ForecastItem";
-import transformForecast from "./../services/transformForecast";
 import "./styles.css";
 
-/*const days = [
-    "Lunes",
-    "Martes",
-    "Miercoles",
-    "Jueves",
-    "Viernes"
-];*/
-
-/*const data = {
-    temperature: 10,
-    humidity: 10,
-    weatherState: 'normal',
-    wind: 'normal'
-}*/
-
-export const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
-export const url= "http://api.openweathermap.org/data/2.5/forecast";
-
-
-class ForecastExtended extends Component {
-    
-    constructor() {
-        super();
-
-        this.state = {
-            forecastData: null
-        }
-
-    }
-
-    componentDidMount() {
-        this.updateCity(this.props.city);
-    }
-
-
-    componentDidUpdate(prevProps, prevState) {
-        if(prevProps.city !== this.props.city) {
-            this.setState({ forecastData: null });
-            this.updateCity(this.props.city);
-        }
-    }
-
-    
-    updateCity = city => {
-        //fetch ot axios 
-        const url_forecast = `${url}?q=${city}&appid=${api_key}`;
-
-        fetch(url_forecast).then(
-            data => (data.json())
-        ).then(
-            weather_data => {
-                console.log(weather_data);
-                const forecastData = transformForecast(weather_data);
-                console.log(forecastData);
-                this.setState({ forecastData });
-            }
-        );
-    }
-
-    renderForecastItenDay(forecastData) {        
-        return forecastData.map( forecast => (
-            <ForecastItem 
-                key={`${forecast.weekDay}${forecast.hour}`}
-                weekDay={forecast.weekDay} 
-                hour={forecast.hour} 
-                data={forecast.data}
-            />
-        ));        
-    }
-
-    renderProgress = () => {
-        return <h3>Cargando pronóstico extendido...</h3>;
-    }
-
-    render() {
-        const { city } = this.props;
-        const { forecastData } = this.state;
-        return (
-            <div>
-                <h2 className="forecast-title">Pronóstico extendido para {city}</h2>
-                { forecastData ?
-                    this.renderForecastItenDay(forecastData) :
-                    this.renderProgress()
-                }
-            </div>
-        )
-    }
+const renderForecastItenDay = (forecastData) => {        
+    return forecastData.map( forecast => (
+        <ForecastItem 
+            key={`${forecast.weekDay}${forecast.hour}`}
+            weekDay={forecast.weekDay} 
+            hour={forecast.hour} 
+            data={forecast.data}
+        />
+    ));        
 }
+
+const renderProgress = () => {
+    return <h3>Cargando pronóstico extendido...</h3>;
+}
+
+const ForecastExtended = ({ city, forecastData }) => (               
+    <div>
+        <h2 className="forecast-title">Pronóstico extendido para {city}</h2>
+        { forecastData ?
+            renderForecastItenDay(forecastData) :
+            renderProgress()
+        }
+    </div>        
+);
 
 ForecastExtended.propTypes =  {
     city: PropTypes.string.isRequired,
+    forecastData: PropTypes.array.isRequired,
 }
 
 export default ForecastExtended;
